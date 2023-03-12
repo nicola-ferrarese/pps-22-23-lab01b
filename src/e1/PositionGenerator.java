bac05068a4fafa9b2332b7c155749835e75e102e
nicola.ferrarese <f.nicola98@gmail.com>
2023-03-12 22:32:05 +0100

    *

--- src/e1/PositionGenerator.java
+++
@@ -1,7 +0,0 @@
-package e1;
-
-public interface PositionGenerator {
-    Pair<Integer, Integer> generateRandomPosition(int size, Pair<Integer, Integer> otherPosition);
-
-    Pair<Integer, Integer> moveKnight(int row, int col, Pair<Integer, Integer> knight);
-}
